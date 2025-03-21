d79aba92cbc9b4e80edb47ac95971a9f06f4f4e9
Salma Ahmed <108466703+Salmaahmed8@users.noreply.github.com>
2025-03-22 00:26:39 +0200

    feat: fetch and display books from Redis

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,24 @@
+import { client } from '@/lib/db'
 import Link from 'next/link'
 
 const getBooks = async () => {
-  
+  const result = await client.zRangeWithScores('books', 0, -1)
+
+  // [{value: 'abc', score: '2397'}, {value: 'def', score: '4233'}]
+
+  // client.hGetAll('books:4235')
+  // client.hGetAll('books:53465')
+  // client.hGetAll('books:76867')
+  // client.hGetAll('books:13643')
+
+  const books = await Promise.all(result.map((b) => {
+    return client.hGetAll(`books:${b.score}`)
+  }))
+  return books
 }
 
 export default async function Home() {
-
+  const books = await getBooks()
   return (
     <main>
       <nav className="flex justify-between">
@@ -13,7 +26,13 @@ export default async function Home() {
         <Link href="/create" className="btn">Add a new book</Link>
       </nav>
       
-      <p>List of books here.</p>
+      {books.map((book) => (
+        <div key={book.title} className='card'>
+          <h2> {book.title}</h2>
+          <p>By {book.author}</p>
+          <p>Rating {book.ratirg}</p>
+        </div>
+      ))}
     </main>
   )
 }
