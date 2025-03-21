b2c7e7666f4985913575dc4108369e8dbd57a005
Salma Ahmed <108466703+Salmaahmed8@users.noreply.github.com>
2025-03-21 23:51:05 +0200

    feat: add createBook Function to Store Book Data in Redis

--- src/app/actions/create.js
+++ src/app/actions/create.js
@@ -6,5 +6,18 @@ import { redirect } from 'next/navigation'
 export async function createBook(formData) {
   const {title, rating, author, blurb} = Object.fromEntries(formData)
 
+  //create book id
+  const id = Math.floor(Math.random() *100000)
+
+  //save new hash for the book
+  await client.hSet(`books:$(id)`, {
+    title,
+    reting,
+    author,
+    blurb
+  })
+
+  redirect('/')
+
   
 }
